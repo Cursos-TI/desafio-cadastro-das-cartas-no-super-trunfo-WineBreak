0169ab6ff5148b2ddd175b52896ad1199be0134e
Guiliana Lima <68544785+WineBreak@users.noreply.github.com>
2025-02-22 18:59:35 -0300

    Update primeiroteste.c

--- primeiroteste.c
+++ primeiroteste.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    //Esse espaço é reservado para declarar as váriaveis
     char estado1[30], estado2[30]; 
     char nome_da_cidade1[30], nome_da_cidade2[30];
     int populacao1, n_pontos_turisticos1; 
@@ -10,26 +11,26 @@ int main() {
 
     printf("\nBEM VINDO AO SUPER TRUNFO PAÍSES!\n");
     printf("\n---------------------------------");
+//Aqui o programa irá começar a pedir os dados da primeira carta do jogo, ler e guardar.
+    printf("\nDigite uma opção de A a H que irá representar um Estado da CARTA 01:"); //Digitar uma sigla de A a H que representa o Estado
+    scanf("%s", &estado1); 
 
-    printf("\nDigite uma opção de A a H que irá representar um Estado da CARTA 01:");
-    scanf("%s", &estado1);
-
-    printf("Digite o nome da Cidade: ");
+    printf("Digite o nome da Cidade: "); //O usuario deve digitar o nome da cidade
     scanf("%s", &nome_da_cidade1);
     
-    printf("Digite a população total:");
+    printf("Digite a população total:"); // O usuario deve digitar a população total da cidade
     scanf("%d", &populacao1);
 
-    printf("Digite a Área:");
+    printf("Digite a Área:"); // O ususario deve digitar a area em km²
     scanf("%f", &area1);
 
-    printf("Digite o PIB: ");
+    printf("Digite o PIB: "); //O ususario deve digitar o PIB da cidade 
     scanf("%f", &pib1);
 
-    printf("Digite o número de Pontos Turisticos: ");
+    printf("Digite o número de Pontos Turisticos: "); // O usuario deve digitar o numero de pontos turisticos
     scanf("%d", n_pontos_turisticos1);
 
-
+//Aqui o programa irá começar a pedir os dados da segunda carta do jogo, ler e guardar.
     printf("\nDigite uma opção de A a H que irá representar um Estado da CARTA 02:");
     scanf("%s", &estado2);
 
@@ -48,7 +49,7 @@ int main() {
     printf("Digite o número de Pontos Turisticos: ");
     scanf("%d", n_pontos_turisticos2);
 
-
+//Aqui o programa irá começar a pedir os dados da primeira carta do jogo, ler e guardar.
     printf("\n");
     printf("\n---------------------------------\n");
     printf("CARTA 01\n");
@@ -60,6 +61,7 @@ int main() {
     printf("\nPIB: %.2f", pib1);
     printf("\nPontos Turisticos: %d", n_pontos_turisticos1);
 
+//Aqui o programa irá começar a pedir os dados da primeira carta do jogo, ler e guardar.
     printf("\n");
     printf("\n---------------------------------\n");
     printf("CARTA 02\n");
